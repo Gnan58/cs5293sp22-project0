8a1bed7980a15abe7200373c10a58041fbaeb9c6
Gnan58 <kolla.gnaneswar@ou.edu>
2022-03-07 21:12:17 -0600

    SSL changes

--- project0/project0.py
+++ project0/project0.py
@@ -1,4 +1,4 @@
-from urllib import request
+import urllib.request
 import tempfile
 import ssl
 import PyPDF2
@@ -13,8 +13,9 @@ def getIncidentsData(url):
     :return: data from a PDF file
     :rtype: <class 'bytes'>
     '''
-    ssl._create_default_https_context = ssl._create_unverified_context
-    data = request.urlopen(url).read()
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"                          
+    data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()                                                                               
     return data
 
 
